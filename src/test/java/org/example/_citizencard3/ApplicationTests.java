afc1c8b13c9f00cc2d991231aa9e34f08875b1c7
alfo0924 <halo0924@yahoo.com.tw>
2024-11-29 21:05:58 +0800

    v1.01 init commit and push

---
+++ src/test/java/org/example/_citizencard3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example._citizencard3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
